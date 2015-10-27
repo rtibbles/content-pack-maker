1175dfccafdc9e77ae7379419e9fca39aea406ce
Aron Fyodor Asor <aronasorman@gmail.com>
2015-10-27 10:46:07 -0700

    Change the use of urllib.retrieve to requests.
    Not only is urllib.retrieve deprecated on python 3, it also doesn't seem
    to follow redirects well.

--- contentpacks/utils.py
+++ contentpacks/utils.py
@@ -1,5 +1,5 @@
 import os
-import urllib.request
+import requests
 
 
 def download_and_cache_file(url, cachedir=None, ignorecache=False) -> str:
@@ -16,6 +16,11 @@ def download_and_cache_file(url, cachedir=None, ignorecache=False) -> str:
     path = os.path.join(cachedir, os.path.basename(url))
 
     if ignorecache or not os.path.exists(path):
-        urllib.request.urlretrieve(url, path)
+        r = requests.get(url, stream=True)
+        r.raise_for_status()
+
+        with open(path, "w") as f:
+            for chunk in r.iter_content(1024, decode_unicode=True):
+                f.write(chunk)
 
     return path
